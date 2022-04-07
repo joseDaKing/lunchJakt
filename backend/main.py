995f771f5a301d909c5e50c1ffe8e447fbf71725
Philip <philip_holmqvist@hotmail.com>
2022-04-07 13:06:10 +0200

    fixat
    ff

--- backend/main.py
+++ backend/main.py
@@ -3,24 +3,37 @@ from scraper import *
 
 app = Flask(__name__)
 
-# För att köra programmet. Öppna powershell, navigera till mappen och skriv
-# $env:FLASK_APP = "main.py"
-# $env:FLASK_ENV = "development"cl
-# flask run
+# För att köra programmet tryck på run knappen.
+# Följ sedan länken som dyker upp i terminalen.
 
 
+#Startsidan
 @app.route("/")
 def home_page():
     return render_template('index.html')
 
-@app.route("/resturant/<name>")
+
+#Användar sidan där man kan se profil.
+@app.route("/user/<input>")
+def user_page():
+    return render_template('user.html')
+
+
+#Resturangsidorna, dynamiska.
+@app.route("/resturant/<name>") 
 def resturant_page(name):
     scrape(name)
-           
     information = read_Content_From_File() 
-    return render_template('index.html', content = information)
+    return render_template('resturant.html', content = information)
+
+
+#Förslagssidan, dynamisk - beror på input som gjorts. 
+@app.route("/suggestions/<input>") 
+def suggestion_page(input):
+    return render_template('suggestion.html')
   
      
+#Main metod för att starta webbservern.     
 if __name__ == '__main__':
     app.run(debug=True, host = '127.0.0.1', port= 8080)
 
