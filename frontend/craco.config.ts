d9c92ed6365a606c9af4ba65e8a069cda38c868f
Yousef Abdulkarim <38055561+joseDaKing@users.noreply.github.com>
2022-04-12 12:02:37 +0200

    Setter up vanilla-extract biblioteket

---
+++ frontend/craco.config.ts
@@ -0,0 +1,17 @@
+import { WebpackOptions } from "@craco/craco";
+
+import { VanillaExtractPlugin } from "@vanilla-extract/webpack-plugin";
+
+
+
+const webpack: WebpackOptions = {
+    plugins: {
+        add: [
+            new VanillaExtractPlugin()
+        ]
+    }
+}
+
+export default {
+    webpack
+}
