057dd175ad05586e4187f26f43cf34fc45d9911d
Yousef Abdulkarim <38055561+joseDaKing@users.noreply.github.com>
2022-04-14 17:08:18 +0200

    Fyllde på teman

--- frontend/src/css/theme.css.ts
+++ frontend/src/css/theme.css.ts
@@ -10,21 +10,36 @@ import { createTheme } from "@vanilla-extract/css";
 
 export const [ theme, themeVars ] = createTheme({
     color: {
-        primary: "",
-        secondary: "",
-        neutral: "",
-        danger: "",
-        success: "",
-        warning: "",
+        primary50: "#f0f9ff",
+        primary100: "#e0f2fe",
+        primary200: "#bae6fd",
+        primary300: "#7dd3fc",
+        primary400: "#38bdf8",
+        primary500: "#0ea5e9",
+        primary600: "#0284c7",
+        primary700: "#0369a1",
+        primary800: "#075985",
+        primary900: "#0c4a6e",
+
+        neutral50: "#f8fafc",
+        neutral100: "#f1f5f9",
+        neutral200: "#e2e8f0",
+        neutral300: "#cbd5e1",
+        neutral400: "#94a3b8",
+        neutral500: "#64748b",
+        neutral600: "#475569",
+        neutral700: "#334155",
+        neutral800: "#1e293b",
+        neutral900: "#0f172a",
     },
     space: {
-        xxs: "",
-        xs: "",
-        sm: "",
-        md: "",
-        lg: "",
-        xl: "",
-        xxl: ""
+        xxs: "8px",
+        xs: "16px",
+        sm: "32px",
+        md: "48px",
+        lg: "64px",
+        xl: "80px",
+        xxl: "96px"
     },
     size: {
         xxs: "",
@@ -48,14 +63,14 @@ export const [ theme, themeVars ] = createTheme({
         full: "100%"
     },
     borderRadius: {
-        xxs: "",
-        xs: "",
-        sm: "",
-        md: "",
-        lg: "",
-        xl: "",
-        xxl: "",
-        round: ""
+        xxs: "2px",
+        xs: "4px",
+        sm: "8px",
+        md: "16px",
+        lg: "32px",
+        xl: "64px",
+        xxl: "128px",
+        round: "9999px"
     },
     borderWidth: {
         sm: "1px",
@@ -70,13 +85,9 @@ export const [ theme, themeVars ] = createTheme({
         87.5: "0.875",
     },
     shadow: {
-        xss: "",
-        xs: "",
-        sm: "",
-        md: "",
-        lg: "",
-        xl: "",
-        xxl: ""
+        sm: "0 .125rem .25rem rgba(0,0,0,.075)",
+        md: "0 .5rem 1rem rgba(0,0,0,.15)",
+        lg: "0 1rem 3rem rgba(0,0,0,.175)",
     },
     zIndex: {
         0: "0",
