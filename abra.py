e8f2aae69b4fc25af94286fe39f92ebcbda2a1dd
Yousef Abdulkarim <38055561+joseDaKing@users.noreply.github.com>
2022-05-23 15:38:26 +0200

    Fia

--- abra.py
+++ abra.py
@@ -4,7 +4,7 @@ from data.find import find
 results = find(
     search_text = "malmö", 
     sort_type = "price", 
-    sort_direction = "price"
+    sort_direction = "ascending"
 )
 
 for r in results:
